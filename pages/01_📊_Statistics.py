0a3850fde504f2e8b992226ee19f9fdbe68804a1
lars geyer <lars.geyer99@gmail.com>
2024-08-29 10:43:56 +0200

    Launch Test

--- pages/01_📊_Statistics.py
+++ pages/01_📊_Statistics.py
@@ -7,9 +7,8 @@ import requests
 st.set_page_config(page_title="Chicago Crime Statistics", page_icon="👮‍♂️")
 
 # API call
-@st.cache_data
-def fetch_crime_data():
-    api_url = "https://chicagocrimes-22489836433.europe-west1.run.app"
-    response = requests.get(api_url)
-    return response.json()
-
+# @st.cache_data
+# def fetch_crime_data():
+#     api_url = "https://chicagocrimes-22489836433.europe-west1.run.app"
+#     response = requests.get(api_url)
+#     return response.json()
